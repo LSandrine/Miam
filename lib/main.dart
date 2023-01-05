a7f1f019265600122f8e7c28fe2a6ce4eb11d0d6
yvanoumbe <tchatagy@3il.fr>
2023-01-05 11:35:14 +0100

    madification du titre

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Miam',
+      title: 'MIAM',
       theme: ThemeData(
         primarySwatch: Colors.lightGreen,
       ),
